3531092c48d2ebbe3ae13ff8a892557f7636edbf
Adreanna <adreannam316@gmail.com>
2022-06-20 12:42:53 -0500

    Reworked simple output layout.
    name input appears on the same line as when prompted to.

--- re_intro/name/name.cpp
+++ re_intro/name/name.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
   string name;
 
-  cout << "Please enter your name: " << endl;
+  cout << "Please enter your name: ";
   cin >> name;
-  cout << "Hello, " << name << "!" << endl;
+  cout << endl << "Hello, " << name << "!" << endl;
   cout << "My name is Wulf!" << endl;
 
   return 0;
